2cc326743ec681c2e1fe44abc98fee6db6be8f03
FARFETCH\tiago.vital <tiago.vital@farfetch.com>
2017-06-15 20:45:21 +0100

    Added DocumentsControllerTests

---
+++ Presentation.API.Tests/Controllers/DocumentsControllerTests.cs
@@ -0,0 +1,36 @@
+﻿namespace Presentation.API.Tests.Controllers
+{
+    using System;
+    using System.Threading.Tasks;
+    using API.Controllers.v1;
+    using Application.Dto;
+    using Application.Services;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Models;
+    using Moq;
+    using Ploeh.AutoFixture;
+
+    [TestClass]
+    public class DocumentsControllerTests
+    {
+        [TestMethod]
+        public async Task AddOrUpdate_ValidIdAndDocument_CallsApplicationService()
+        {
+            // Arrange
+            var service = new Mock<IDocumentService>();
+
+            var target = new DocumentsController(service.Object);
+
+            var fixture = new Fixture();
+
+            var model = fixture.Create<AddOrUpdateDocumentModel>();
+            var documentId = Guid.NewGuid();
+
+            // Act
+            await target.AddOrUpdate(documentId, model);
+
+            // Assert
+            service.Verify(i => i.Save(documentId, It.IsAny<DocumentDto>()));
+        }
+    }
+}
---
+++ Presentation.API.Tests/TestsSetup.cs
@@ -0,0 +1,29 @@
+﻿namespace Presentation.API.Tests
+{
+    using System.Diagnostics.CodeAnalysis;
+    using AutoMapper;
+    using Infrastructure.CrossCutting.Automapper;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Presentation.API.MapperProfiles;
+
+    [ExcludeFromCodeCoverage]
+    [TestClass]
+    public class TestsSetup
+    {
+        /// <summary>
+        /// The setup.
+        /// </summary>
+        /// <param name="context"></param>
+        [AssemblyInitialize]
+        public static void Setup(TestContext context)
+        {
+            var profileLoader = new StaticProfileLoader(
+                new Profile[]
+                {
+                    new DocumentsProfile()
+                });
+
+            MapperConfig.Initialize(profileLoader);
+        }
+    }
+}
