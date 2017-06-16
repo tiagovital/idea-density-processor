8bfff2ccc88d49bd8f2e30d0c9fcf06eceb39eba
FARFETCH\tiago.vital <tiago.vital@farfetch.com>
2017-06-16 01:24:13 +0100

    implemented MongoRepository

--- Data.Repository/Repositories/MongoRepository.cs
+++ Data.Repository/Repositories/MongoRepository.cs
@@ -1,6 +1,97 @@
 ﻿namespace Data.Repository
 {
-    public abstract class MongoRepository
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Data.Repository.Specifications;
+    using Domain.Model;
+    using Extensions;
+    using MongoDB.Driver;
+
+    public class MongoRepository<TEntity> : IRepository<TEntity> where TEntity : IEntity
     {
+        private readonly IMongoClient client;
+
+        public MongoRepository(IMongoClient client)
+        {
+            this.client = client;
+        }
+
+        public async Task Delete(Guid id)
+        {
+            await this.client
+                .GetDatabase("IDP")
+                .GetCollectionFor<TEntity>()
+                .DeleteOneAsync(x => x.Id == id)
+                .ConfigureAwait(false);
+        }
+
+        public async Task<TEntity> Find(ISpecification<TEntity> specification)
+        {
+            var filter = new FilterDefinitionBuilder<TEntity>().Where(m => specification.IsSatisfiedBy(m));
+
+            return await this.client
+                .GetDatabase("IDP")
+                .GetCollectionFor<TEntity>()
+                .Find(filter)
+                .FirstOrDefaultAsync()
+                .ConfigureAwait(false);
+        }
+
+        public Task<IEnumerable<TEntity>> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<TEntity>> FindAll(ISpecification<TEntity> specification, int page, int pageSize)
+        {
+            var filter = new FilterDefinitionBuilder<TEntity>().Where(m => specification.IsSatisfiedBy(m));
+
+            return await this.client
+                .GetDatabase("IDP")
+                .GetCollectionFor<TEntity>()
+                .Find(filter)
+                .Skip(page - 1)
+                .Limit(pageSize)
+                .ToListAsync()
+                .ConfigureAwait(false);
+        }
+
+        public async Task<TEntity> Get(Guid id)
+        {
+            return await this.client
+                .GetDatabase("IDP")
+                .GetCollectionFor<TEntity>()
+                .Find(x => x.Id == id)
+                .SingleOrDefaultAsync()
+                .ConfigureAwait(false);
+        }
+
+        public Task<IEnumerable<TEntity>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAll(int page, int pageSize)
+        {
+            return await this.client
+                .GetDatabase("IDP")
+                .GetCollectionFor<TEntity>()
+                .Find(FilterDefinition<TEntity>.Empty)
+                .Skip(page - 1)
+                .Limit(pageSize)
+                .ToListAsync()
+                .ConfigureAwait(false);
+        }
+
+        public async Task Save(TEntity entity)
+        {
+            var opts = new UpdateOptions { IsUpsert = true };
+
+            await this.client
+                .GetDatabase("IDP")
+                .GetCollectionFor<TEntity>()
+                .ReplaceOneAsync(x => x.Id == entity.Id, entity, opts);
+        }
     }
 }
---
+++ Domain.Model/IEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Model
+{
+    using System;
+
+    public interface IEntity
+    {
+        Guid Id { get; set; }
+
+        DateTime CreatedAt { get; set; }
+
+        string CreatedBy { get; set; }
+    }
+}
